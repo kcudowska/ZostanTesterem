f2a3eba8ba99c1181a6178f21632dcf292e6418a
Katarzyna Cudowska <cudowska.katarzyna@gmail.com>
2023-04-02 16:47:52 +0200

    Third test added

--- src/test/java/PageSetup.java
+++ src/test/java/PageSetup.java
@@ -21,6 +21,7 @@ public class PageSetup {
     @AfterEach
     public void teardown() {
         //różnica między driver.close() a driver.quit()
-      //  driver.quit();
+        // Ctrl + /
+        driver.quit();
     }
 }
